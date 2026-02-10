867331a293ed1c852bfc03f4a04b6b2df59aee7e
marat_danyarov <maratdanyarov87@gmail.com>
2026-02-10 12:38:19 +0500

    update forms

--- users/forms.py
+++ users/forms.py
@@ -4,9 +4,25 @@ from .models import User
 
 
 class RegisterForm(UserCreationForm):
+    """User registration form with email and phone fields."""
+
     email = forms.EmailField(required=True)
     phone = forms.CharField(max_length=20, required=False)
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2')
+        fields = ("username", "email", "password1", "password2", "phone")
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    """Form for updating user profile information."""
+
+    class Meta:
+        model = User
+        fields = ("first_name", "last_name", "email", "phone")
+        widgets = {
+            "first_name": forms.TextInput(attrs={"class": "form-control"}),
+            "last_name": forms.TextInput(attrs={"class": "form-control"}),
+            "email": forms.EmailInput(attrs={"class": "form-control"}),
+            "phone": forms.TextInput(attrs={"class": "form-control"}),
+        }
